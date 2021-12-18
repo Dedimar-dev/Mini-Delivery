5976bffa90905d93a6c65203dffb9f83bab7be71
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2021-12-18 17:50:41 -0300

    Cadastro de usuário

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,17 +1,44 @@
+const knex = require('../bancodedados/conexao');
+const {
+  cadastroUsuarioSchema,
+  atualizacaoUsuarioSchema
+} = require('../validacoes/usuarioSchema');
 
 const cadastroUsuario = async (req, res) => {
   const {
     nome,
     email,
     senha,
+    nome_lanchonete
   } = req.body
-  
+
   try {
-    res.status(200).json(req.body);
-  } catch (error) {
-    console.log(error.message)
+    await cadastroUsuarioSchema.validate(req.body);
+
+    const emailExistente = knex('usuarios')
+      .where({ email })
+      .first()
+
+    if (emailExistente) {
+      return res.status(400).json({
+        message:'Email já está sendo usado por outro usuário.'
+      })
+    }
+
+      const usuarioCadastrado = knex('usuarios')
+        .insert({
+          nome,
+          email,
+          senha,
+          nome_lanchonete
+        });
+    res.status(200).json({
+      message: 'Cadastrado concluído com sucesso.'
+    });
+  } catch ({message}) {
+    console.log(message)
   }
- 
+
 }
 
 module.exports = {
