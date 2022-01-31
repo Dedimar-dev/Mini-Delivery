1c9b5147749301cb4b40ae38044943a74c07be2e
Dedimar <85937748+Dedimar-dev@users.noreply.github.com>
2022-01-31 17:45:56 -0300

    Implementação de novas rotas/ usando o filtro

--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const produtos = require('./controladores/produtos');
+const usuarios = require('./controladores/usuarios');
+const login = require('./controladores/login');
+const verificaToken = require('./filtros/verificaToken')
 
 const rotas = express();
 
+rotas.post('/usuarios', usuarios.cadastroUsuario);
+rotas.post('/login', login.login);
+
+rotas.use(verificaToken);
+
+rotas.put('/usuarios', usuarios.atualizarUsuario);
 rotas.post('/produtos', produtos.cadastroProduto);
 rotas.get('/produtos', produtos.listarProdutos);
 rotas.get('/produtos/:nome', produtos.pesquisarProdutos);
